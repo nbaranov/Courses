0e68e955797c6a02815d42acd7b8dd1f84047a14
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-08-28 20:23:44 +0300

    added pagination on page of products

--- Module_D/D4/project/simpleapp/views.py
+++ Module_D/D4/project/simpleapp/views.py
@@ -1,15 +1,20 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
-from .models import Product
+
 from datetime import datetime
 
- 
- 
+from .models import Product
+
+
+#Создаем свои вьюшки
+
 class ProductsList(ListView):
-    model = Product  # указываем модель, объекты которой мы будем выводить
-    template_name = 'products.html'  # указываем имя шаблона, в котором будет лежать html, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
-    context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через html-шаблон
-    
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+    ordering = ['-price']
+    paginate_by = 2
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["time_now"] = datetime.utcnow()
