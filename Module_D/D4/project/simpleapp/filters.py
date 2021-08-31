ef8fc067bcdab2099b20edefc52330edecc40a8f
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-08-31 21:32:06 +0300

    created myself product filters using docementary

--- Module_D/D4/project/simpleapp/filters.py
+++ Module_D/D4/project/simpleapp/filters.py
@@ -1,12 +1,41 @@
 from django_filters import FilterSet
-from .models import Product
+import django_filters
+from .models import Category, Product
 
 #создаем фильтр
+#Попытка сдлеать нормальную форму по документации
 class ProductFilter(FilterSet):
+    CHOISE_CATEGORYES = Category.objects.all()
+    name = django_filters.CharFilter(field_name="name",
+                                             lookup_expr='icontains',
+                                             label="Наименование",)
+    category = django_filters.ModelChoiceFilter()
+    category.label = "Категория"
+    category.field_name = 'category'
+    category.queryset = Category.objects.all()
+    
+
+    price_gt = django_filters.NumberFilter(field_name="price",
+                                           lookup_expr="gt",
+                                           label="Цена от")
+    price_lt = django_filters.NumberFilter(field_name="price",
+                                           lookup_expr="lt",
+                                           label="Цена до")
+    
     class Meta:
-        model = Product
-        exclude = ['quantity']
-        # interface = ('relay.Node',)
-        fields = {'name' : ['contains'], 
-                #   'category' : [], 
-                  'price' : ['lt']}
+          model = Product
+          exclude = ['quantity']
+          fields = ['name', 'category', 'price_gt', 'price_lt']
+          
+
+
+
+# Пример из курса    
+# class ProductFilter(FilterSet):
+#     class Meta:
+#         model = Product
+#         exclude = ['quantity']
+#         # fields = {'name' : ['icontains'], 
+#         #         #   'category' : [], 
+#         #           'price' : ['lt']}
+#         fields = ['name', 'category', 'price']
