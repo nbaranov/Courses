3f39618a1dcb036e181c0a7ae54776e55bf41ccd
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-09-20 15:11:19 +0300

    add some news, makemigrations

--- Module_D/NewsPaper/news/admin.py
+++ Module_D/NewsPaper/news/admin.py
@@ -1,7 +1,8 @@
-from .models import Post, PostCategory, Comment
+from .models import Post, PostCategory, Comment, Author
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
+admin.site.register(Author)
