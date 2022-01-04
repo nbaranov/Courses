fe91638ba8bd1ae42a5e839413dbc4bc4d7b5011
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-01-04 13:24:51 +0300

    restat module D

--- Module_D/NewsPaper/news/admin.py
+++
@@ -1,8 +0,0 @@
-from .models import Post, PostCategory, Comment, Author
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(PostCategory)
-admin.site.register(Comment)
-admin.site.register(Author)
--- Module_D/NewsPaper/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import IndividualNews, PostList, NewsListSearch
-
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>', IndividualNews.as_view()),
-    path('search', NewsListSearch.as_view())
-]
-
