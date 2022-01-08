58af1f4c178afe424832c489d00a5cff499febb4
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-01-08 12:30:03 +0300

    Complete unit D2.6

--- module_D/D2/McDonalds/restorant/models.py
+++ module_D/D2/McDonalds/restorant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class Product(models.Model):
@@ -30,6 +31,9 @@ class Staff(models.Model):
     )
     labor_contract = models.IntegerField()
 
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
 
 class Order(models.Model):
     time_in = models.DateTimeField(auto_now_add=True)
@@ -37,12 +41,36 @@ class Order(models.Model):
     cost = models.FloatField(default=0.0)
     take_away = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
-    
+
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return round((self.time_out - self.time_in).total_seconds()/60)
+        else:
+            return round((datetime.now() - self.time_in).total_seconds()/60)
 
 
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+    _amount = models.IntegerField(default=1, db_column='amount')
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self._amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
