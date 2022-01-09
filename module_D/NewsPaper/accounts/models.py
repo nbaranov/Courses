5ad0fd90af431a414ba05011181cb38c59e1c36a
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-01-09 19:26:34 +0300

    Complete module D2

--- module_D/NewsPaper/accounts/models.py
+++ module_D/NewsPaper/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from news.models import Post
+from news.models import Comment
+
+class Author(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE)
+    rating = models.FloatField(default=0.0)
+
+    def update_rating(self):
+        new_rating = 0
+        author_posts = Post.objects.filter(author=self)
+        author_comments = Comment.objects.filter(user=self.user)
+        for post in author_posts:
+            new_rating += post.rating * 3
+            post_comments = Comment.objects.filter(post=post)
+            for comment in post_comments:
+                if comment not in author_comments:
+                    new_rating += comment.rating
+        for comment in author_comments:
+            new_rating += comment.rating
+
+        self.rating = new_rating
+        self.save()
