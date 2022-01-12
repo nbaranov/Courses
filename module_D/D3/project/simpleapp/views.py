fbab00c2deb52019fb13308605bd714a6e2fef25
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-01-12 22:08:36 +0300

    add DetailView for products, add urls, add templates

--- module_D/D3/project/simpleapp/views.py
+++ module_D/D3/project/simpleapp/views.py
@@ -1,6 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import \
-    ListView  # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
@@ -8,3 +7,10 @@ class ProductsList(ListView):
     model = Product  # указываем модель, объекты которой мы будем выводить
     template_name = 'product_list.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class ProductDetail(DetailView):
+    model = Product # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'product.html' # название шаблона будет product.html
+    context_object_name = 'product' # название объекта
