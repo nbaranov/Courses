85ae7b2aab68e75c38573abcec4df07909ead152
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-01-17 08:23:30 +0300

    add urls and views for news, add templates

--- module_D/NewsPaper/news/views.py
+++ module_D/NewsPaper/news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+class NewsListView(ListView):
+    model = Post
+    template_name = 'news_list.html'
+    context_object_name = 'news'
+
+
