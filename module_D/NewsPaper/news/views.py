9a3db0a8788de8b6227b5e4def9be1e78e1710ae
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-02-25 12:42:12 +0300

    Complete unit D3.3

--- module_D/NewsPaper/news/views.py
+++ module_D/NewsPaper/news/views.py
@@ -1,12 +1,20 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 # Create your views here.
 class NewsListView(ListView):
     model = Post
     template_name = 'news_list.html'
     context_object_name = 'news'
+    ordering = ['-id']
+    # queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now()
+        return context
 
 
 class NewsDetail(DetailView):
