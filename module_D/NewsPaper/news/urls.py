69e4fdcc4f0352cf5d68c12182d0c300cf649ea0
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-02-26 20:46:34 +0300

    added django-filters, created filter for news, added page with news search

--- module_D/NewsPaper/news/urls.py
+++ module_D/NewsPaper/news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import  path
-from .views import NewsListView, NewsDetail
+from .views import NewsListView, NewsDetail, NewsSearchView
 
 urlpatterns = [
     path('', NewsListView.as_view()),
     path('<int:pk>', NewsDetail.as_view()),
+    path('search/', NewsSearchView.as_view())
 ]
--- module_D/NewsPaper/news/views.py
+++ module_D/NewsPaper/news/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import SearchNewsFilter
+
 from datetime import datetime
 
 # Create your views here.
@@ -8,8 +10,7 @@ class NewsListView(ListView):
     model = Post
     template_name = 'news_list.html'
     context_object_name = 'news'
-    ordering = ['-id']
-    # queryset = Post.objects.order_by('-id')
+    queryset = Post.objects.order_by('-id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,3 +22,15 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'post'
+
+
+class NewsSearchView(ListView):
+    model = Post
+    template_name = 'search_news.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now()
+        context['filter'] = SearchNewsFilter(self.request.GET, queryset=self.get_queryset())
+        return context
