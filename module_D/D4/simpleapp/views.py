56339be37c186a26e7e3b72a0748726ee0ed489e
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-03-01 19:04:36 +0300

    complete unit D4.3 simpleapp examples

--- module_D/D4/simpleapp/views.py
+++ module_D/D4/simpleapp/views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView
 
-from .models import Product
+from .models import Product, Category
 from .filters import ProductFilter # импортируем недавно написанный фильтр
+from .forms import ProductForm # импортируем нашу форму
 
 class ProductsList(ListView):
     model = Product  # указываем модель, объекты которой мы будем выводить
@@ -10,6 +11,7 @@ class ProductsList(ListView):
     context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
     ordering = ['-price']
     paginate_by = 2 # Элементов на странице
+    form_class = ProductForm # добавляем форм класс, чтобы получать доступ к форме через метод POST
 
     # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словари и есть переменные, к которым мы сможем потом обратиться через шаблон
     def get_context_data(self, **kwargs):
@@ -17,8 +19,17 @@ class ProductsList(ListView):
         context['time_now'] = datetime.now() # добавим переменную текущей даты time_now
         context['ads'] = None # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
         context['filter'] = ProductFilter(self.request.GET, queryset=self.get_queryset()) # вписываем наш фильтр в контекст
+        context['categories'] = Category.objects.all()
+        context['form'] = ProductForm()
         return context
 
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST) # создаём новую форму, забиваем в неё данные из POST-запроса 
+ 
+        if form.is_valid(): # если пользователь ввёл всё правильно и нигде не накосячил, то сохраняем новый товар
+            form.save()
+ 
+        return super().get(request, *args, **kwargs)
 
 
 
