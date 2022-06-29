249b13b59f13417605757aeb48e9751581fcae76
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-06-29 14:00:49 +0300

    Comlete unit D2.5

---
+++ module_D/d2/McDonalds.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    price = models.FloatField(default = 0.0)
+
+
+class Stuff(models.Model):
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
+    POSITIONS = [
+        (director, "Директор"),
+        (admin, "Администратор"),
+        (cook, "Повар"),
+        (cashier, "Кассир"),
+        (cleaner, "Уборщик"),
+    ]
+
+    full_name = models.CharField(max_length = 255)
+    position = models.CharField(max_length = 2,
+                                choices = POSITIONS,
+                                default = cashier)
+    labor_contract = models.IntegerField()
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add = True)
+    time_out = models.DateTimeField(null = True)
+    cost = models.FloatField(default = 0.0)
+    take_away = models.BooleanField(default = False)
+    complete = models.BooleanField(default = False)
+    staff = models.ForeignKey(Stuff, on_delete = models.CASCADE)
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE),
+    in_order = models.ForeignKey(Order, on_delete = models.CASCADE)
+    amount = models.IntegerField(default = 1)
+
