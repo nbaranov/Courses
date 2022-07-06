a1356aa9f7181c84dbffc4596453f049d5abdcd5
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-07-06 20:55:00 +0300

    complete unit D3.3 tutorial

--- module_D/d3/project/simpleapp/views.py
+++ module_D/d3/project/simpleapp/views.py
@@ -1,12 +1,20 @@
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView # импортируем класс получения деталей объекта
 from .models import Product
- 
+from datetime import datetime 
  
 class ProductList(ListView):
     model = Product  
     template_name = 'products.html'
     context_object_name = 'products'
+    queryset = Product.objects.order_by('-id')
+
+    # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словари и есть переменные, к которым мы сможем потом обратиться через шаблон
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.now() # добавим переменную текущей даты time_now
+        context['ads1'] = None # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
  
  
 # создаём представление, в котором будут детали конкретного отдельного товара
