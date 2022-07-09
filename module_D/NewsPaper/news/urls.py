641fe92d76e824b25ed202c9c92a5945b7da2219
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-07-09 20:19:14 +0300

    complete unit D4.2 tasks

--- module_D/NewsPaper/news/urls.py
+++ module_D/NewsPaper/news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewsListView, NewsDetail
+from .views import NewsListView, NewsDetail, NewsSearchView
 
 
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
-    path('<int:pk>', NewsDetail.as_view(), name='post_detail')
+    path('<int:pk>', NewsDetail.as_view(), name='post_detail'),
+    path('search/', NewsSearchView.as_view(), name='search_news')
 ]
--- module_D/NewsPaper/news/views.py
+++ module_D/NewsPaper/news/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
 from .models import Post
+from .filters import SearchNewsFilter
 
 
 # Create your views here.
@@ -15,3 +17,15 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'news/post_detail.html'
     context_object_name = 'post'
+
+
+
+class NewsSearchView(ListView):
+    model = Post
+    template_name = 'news/search_news.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = SearchNewsFilter(self.request.GET, queryset=self.get_queryset())
+        return context
