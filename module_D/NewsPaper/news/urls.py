784faca1e9d3001eab6cc7bd1f9475f0f5c85d2c
Nikita Baranov <nbaranov.vrn@gmail.com>
2022-08-11 17:09:06 +0300

    Complete unit D4 tasks

--- module_D/NewsPaper/news/urls.py
+++ module_D/NewsPaper/news/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import NewsListView, NewsDetail, NewsSearchView
+from .views import NewsListView, NewsDetail, NewsSearchView 
+from .views import PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
     path('<int:pk>', NewsDetail.as_view(), name='post_detail'),
-    path('search/', NewsSearchView.as_view(), name='search_news')
+    path('search/', NewsSearchView.as_view(), name='search_news'),
+    path('add/', PostCreateView.as_view(), name='create_post'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='update_post'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post'),
 ]
--- module_D/NewsPaper/news/views.py
+++ module_D/NewsPaper/news/views.py
@@ -1,8 +1,9 @@
 #from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 from .filters import SearchNewsFilter
+from .forms import PostForm
 
 
 # Create your views here.
@@ -24,8 +25,30 @@ class NewsSearchView(ListView):
     model = Post
     template_name = 'news/search_news.html'
     context_object_name = 'post'
+    queryset = Post.objects.order_by('-id')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = SearchNewsFilter(self.request.GET, queryset=self.get_queryset())
         return context
+
+# дженерик для создания объекта. Надо указать только имя шаблона и класс формы который мы написали в прошлом юните. Остальное он сделает за вас
+class PostCreateView(CreateView):
+    template_name = 'news/create_post.html'
+    form_class = PostForm
+
+# дженерик для редактирования объекта
+class PostUpdateView(UpdateView):
+    template_name = 'news/create_post.html'
+    form_class = PostForm
+ 
+    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+# дженерик для удаления товара
+class PostDeleteView(DeleteView):
+    template_name = 'news/delete_post.html'
+    queryset = Post.objects.all()
+    success_url = '/products/'
